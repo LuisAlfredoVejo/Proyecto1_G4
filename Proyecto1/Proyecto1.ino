063d2e7d52a714862d9eaf78041e1845758f40c5
Luis Alfredo Vejo Mendoza <2205483820101@ingenieria.usac.edu.gt>
2021-08-30 16:58:16 -0600

    Se agregaron metodos para controlar las funciones del carrito

--- Proyecto1/Proyecto1.ino
+++ Proyecto1/Proyecto1.ino
@@ -1,7 +1,35 @@
+char lectura = Serial1.read();
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  Serial1.begin(9600);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
   }
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(12,LOW);
+  digitalWrite(13,LOW);
+  delay(50);
  }
+
+void velocidad(){
+  
+}
+
+void izquierda(){
+  
+}
+
+void derecha(){
+  
+}
+
+void adelante_reversa(){
+  
+}
+
+void detenerse(){
+  
+}
